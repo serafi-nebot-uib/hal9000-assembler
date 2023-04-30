28e9d72f510adf889cc53653e3c8829cfb3f573f
hexdhog <hexdhog@gmail.com>
2023-04-30 20:04:35 +0200

    added instruction display

--- compiler.py
+++ compiler.py
@@ -26,19 +26,21 @@ if __name__ == "__main__":
             "--output",
             help=f"File output path",
             default="stdout")
-    parser.add_argument("--hex", action=argparse.BooleanOptionalAction)
-    parser.add_argument("--bin", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--display-instruction", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--display-hex", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--display-bin", action=argparse.BooleanOptionalAction)
 
     args = vars(parser.parse_args())
 
     input_file_path = args["file"]
     output_file_path = args["output"]
-    output_hex = args["hex"]
-    output_bin = args["bin"]
+    output_instruction = args["display_instruction"]
+    output_hex = args["display_hex"]
+    output_bin = args["display_bin"]
 
-    output_opts = (output_hex, output_bin)
+    output_opts = (output_instruction, output_hex, output_bin)
     if not any(output_opts):
-        output_hex, output_bin = [True] * len(output_opts)
+        output_instruction, output_hex, output_bin = [True] * len(output_opts)
 
     if input_file_path is None:
         parser.print_help()
@@ -60,6 +62,9 @@ if __name__ == "__main__":
                 instr = instr_for_mnemonic(*tokens)
                 data = instr.encode()
 
+                if output_instruction:
+                   fout.write(l.strip("\n").ljust(16))
+
                 if output_bin:
                     fout.write(f"{int.from_bytes(data):016b} ")
 
@@ -74,6 +79,6 @@ if __name__ == "__main__":
         if fin is not None:
             fin.close()
 
-        if fout is not None:
+        if fout is not sys.stdout:
             fout.close()
 
